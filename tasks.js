5f8e06b30f289987eca41bbd4bf23cab3cda2bf0
sizzurp-101-69 <evgeniylukin2005@gmail.com>
2026-06-08 15:19:57 +0700

    upd

--- tasks.js
+++ tasks.js
@@ -33,3 +33,40 @@ function isValidDate(dateStr) {
     return d.toISOString().slice(0, 10) === dateStr
 }
 
+function main() {
+    const rl = readline.createInterface({ input, output })
+    await loadTasks()
+
+    console.log('===Добро пожаловать в Терминальный Планировщик Задач ===')
+
+    while (true)
+        console.log('\nДоступные команды')
+    console.log('1. Просмотр задач')
+    console.log('2. Добавление задачи')
+    console.log('3. Редактирование задачи')
+    console.log('4. Удаление задачи')
+    console.log('5. Выход')
+
+    const choice = await rl.question('\nВыберите действие (1-5): ')
+
+    switch (choice.trim()) {
+        case '1':
+            await viewTasksMenu(rl)
+            break
+        case '2':
+            await addTaskMenu(rl)
+            break
+        case '3':
+            await editTaskMenu(rl)
+            break
+        case '4':
+            await deleteTaskMenu(rl)
+            break
+        case '5':
+            console.log('До свидания!')
+            rl.close()
+            return
+            default
+            console.log('Неверный ввод. Пожалуйста, выберите пункт от 1 до 5.')
+    }
+}
