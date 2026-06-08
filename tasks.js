d6ae9d238750e48d3991e84e4e6f98f3856dcc97
sizzurp-101-69 <evgeniylukin2005@gmail.com>
2026-06-08 15:02:11 +0700

    upd

--- tasks.js
+++ tasks.js
@@ -18,6 +18,18 @@ function loadTasks() {
 
 function saveTasks() {
     try {
-        
+        await fs.writeFile(FILE_PATH, JSON.stringify(tasks, null, 2) , 'utf-8')
+    } catch (error) {
+        console.error('Ошибка при чтении задач:', error.message)
     }
 }
+
+function isValidDate(dateStr) {
+    const regEx = /^\d{4}-\d{2}$/
+    if (!dateStr.match(regEx)) return false
+    const d = new Date (dateStr)
+    const dNum = d.getTime()
+    if (!dNum && dNum !== 0) return false
+    return d.toISOString().slice(0, 10) === dateStr
+}
+
